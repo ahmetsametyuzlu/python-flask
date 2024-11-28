b6ac59f6051ee7d2dc561a2f24e3e53a1311ff71
Ahmet Samet Yüzlü <ahmetsametyuzlu@gmail.com>
2024-11-28 17:31:00 +0300

    Added new files.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from data import fetch_dersler
+from data import fetch_dersler, fetch_ogrenciler
 
 app = Flask(__name__)
 
@@ -23,3 +23,8 @@ def course():
     dersler = fetch_dersler()
     return render_template("course.html", dersler=dersler)
 
+
+@app.route("/ogrenciler")
+def student():
+    ogrenciler = fetch_ogrenciler()
+    return render_template("student.html", ogrenciler=ogrenciler)
--- data.py
+++ data.py
@@ -11,3 +11,15 @@ def fetch_dersler():
     connection.close()
     return results
 
+
+def fetch_ogrenciler():
+    connection = sqlite3.connect("db.sqlite")  # Replace with your SQLite database file
+    connection.row_factory = sqlite3.Row  # To fetch rows as dictionaries
+    cursor = connection.cursor()
+    query = "SELECT * FROM ogrenciler;"  # Assuming 'dersler' is a table in your database
+    cursor.execute(query)
+    results = [dict(row) for row in cursor.fetchall()]  # Convert rows to dictionaries
+    cursor.close()
+    connection.close()
+    return results
+
